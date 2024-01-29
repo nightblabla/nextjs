90bbe4549e5bbc11a418a79e3bbf4f2cbf905625
nightblabla <eminc2033@gmail.com>
2024-01-29 22:47:34 +0300

    type error

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -6,5 +6,7 @@ const handler = NextAuth(authOptions)
 
 // export const GET = (req: NextApiRequest, res: NextApiResponse) => handler(req, res);
 // export const POST = (req: NextApiRequest, res: NextApiResponse) => handler(req, res);
-
-export default handler;
+export default async function routeHandler(req: NextApiRequest, res: NextApiResponse) {
+    await handler(req, res);
+  }
+  
