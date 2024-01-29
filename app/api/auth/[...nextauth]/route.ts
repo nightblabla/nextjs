81f04be723d0e45596e3d7902c5aa94241325368
nightblabla <eminc2033@gmail.com>
2024-01-29 23:08:11 +0300

    type error

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,9 @@
 import { authOptions } from "@/lib/auth";
 import { NextApiRequest, NextApiResponse } from "next";
+import { Request as ExpressRequest, Response as ExpressResponse } from 'express';
 import NextAuth from "next-auth"
 
 const handler = NextAuth(authOptions)
 
-export const GET = (req: NextApiRequest, res: NextApiResponse) => handler(req, res);
-export const POST = (req: NextApiRequest, res: NextApiResponse) => handler(req, res);
+export const GET = (req: Request, res: Response) => handler(req, res);
+export const POST = (req: Request, res: Response) => handler(req, res);
