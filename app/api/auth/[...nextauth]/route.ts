d7d92ad6791108bfe9cfbf8ce1a2c15bd6316344
nightblabla <eminc2033@gmail.com>
2024-01-29 23:26:50 +0300

    type error

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,5 +4,13 @@ import NextAuth from "next-auth"
 
 const handler = NextAuth(authOptions)
 
-export const GET = (req: NextApiRequest, res: NextApiResponse) => handler(req, res);
-export const POST = (req: NextApiRequest, res: NextApiResponse) => handler(req, res);
+// export const GET = (req: NextApiRequest, res: NextApiResponse) => handler(req, res);
+// export const POST = (req: NextApiRequest, res: NextApiResponse) => handler(req, res);
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    if (req.method === 'GET' || req.method === 'POST') {
+      await handler(req, res);
+    } else {
+      res.setHeader('Allow', ['GET', 'POST']);
+      res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
+  };
