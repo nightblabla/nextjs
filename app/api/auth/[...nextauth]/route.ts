ca85a395fb5753fcb8e3c984d1309d574fdcf075
nightblabla <eminc2033@gmail.com>
2024-01-29 23:32:56 +0300

    type error

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -6,11 +6,16 @@ const handler = NextAuth(authOptions)
 
 // export const GET = (req: NextApiRequest, res: NextApiResponse) => handler(req, res);
 // export const POST = (req: NextApiRequest, res: NextApiResponse) => handler(req, res);
-export const otherHandler =async (req: NextApiRequest, res: NextApiResponse) => {
-    if (req.method === 'GET' || req.method === 'POST') {
-      await handler(req, res);
-    } else {
-      res.setHeader('Allow', ['GET', 'POST']);
-      res.status(405).end(`Method ${req.method} Not Allowed`);
+export const yourApiRouteHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+    try {
+      if (req.method === 'GET' || req.method === 'POST') {
+        await handler(req, res);
+      } else {
+        res.setHeader('Allow', ['GET', 'POST']);
+        res.status(405).end(`Method ${req.method} Not Allowed`);
+      }
+    } catch (error) {
+      console.error('Error handling request:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
     }
   };
