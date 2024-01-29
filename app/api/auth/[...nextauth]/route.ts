63e323946b4ad7584616be1488135f7979affc90
emre <eminc2033@gmail.com>
2024-01-29 22:37:40 +0300

    Initial commit

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,5 +4,8 @@ import NextAuth from "next-auth"
 
 const handler = NextAuth(authOptions)
 
-export const GET = (req: NextApiRequest, res: NextApiResponse) => handler(req, res);
-export const POST = (req: NextApiRequest, res: NextApiResponse) => handler(req, res);
+// export const GET = (req: NextApiRequest, res: NextApiResponse) => handler(req, res);
+// export const POST = (req: NextApiRequest, res: NextApiResponse) => handler(req, res);
+export default async function routeHandler(req: NextApiRequest, res: NextApiResponse) {
+    await handler(req, res);
+  }
