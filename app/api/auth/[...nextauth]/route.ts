cbbf457a7d40a225f10b99bbcba6fc4e7189ef3e
nightblabla <eminc2033@gmail.com>
2024-01-29 22:55:24 +0300

    type error

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,14 +4,5 @@ import NextAuth from "next-auth"
 
 const handler = NextAuth(authOptions)
 
-// export const GET = (req: NextApiRequest, res: NextApiResponse) => handler(req, res);
-// export const POST = (req: NextApiRequest, res: NextApiResponse) => handler(req, res);
-
-export default async function routeHandler(req: NextApiRequest, res: NextApiResponse) {
-    try {
-      await handler(req, res);
-    } catch (error) {
-      console.error(error);
-      res.status(500).end("Internal Server Error");
-    }
-  }
+export const GET = (req: NextApiRequest, res: NextApiResponse) => handler(req, res);
+export const POST = (req: NextApiRequest, res: NextApiResponse) => handler(req, res);
